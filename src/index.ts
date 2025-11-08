092811dc8e2a010684238eb4983da4df3dcfb6f4
Rimfis <dimitri.rim@yandex.ru>
2025-11-08 21:55:59 +0300

    feat: add types and data users

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,22 @@
 import dotenv from 'dotenv';
+import { createServer } from 'node:http';
+import { isValidUrl } from './utils/utils';
 
 dotenv.config();
 
-console.log(process.env.PORT);
+const PORT = process.env.PORT ? Number.parseInt(process.env.PORT) : 3000;
+
+const server = createServer((req, res) => {
+  const { method, url } = req;
+
+  if (method && url && isValidUrl(method, url)) {
+    console.log(method, url);
+  }
+
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World!\n');
+});
+
+server.listen(PORT, '127.0.0.1', () => {
+  console.log(`Listening on 127.0.0.1:${PORT}`);
+});
