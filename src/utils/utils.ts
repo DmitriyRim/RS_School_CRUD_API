a2e941fa0b4ebdff0b290726beee5a479c05bce0
Rimfis <dimitri.rim@yandex.ru>
2025-11-08 21:52:02 +0300

    feat: add isValidUrl in utils

---
+++ src/utils/utils.ts
@@ -0,0 +1,16 @@
+export const isValidUrl = (method: string, url: string) => {
+  const baseApiUrl = '/api/users';
+  const parseUrl = url.split('/');
+
+  if (
+    (['GET', 'POST'].includes(method) && baseApiUrl === url) ||
+    (['GET', 'PUT', 'DELETE'].includes(method) &&
+      baseApiUrl.startsWith(baseApiUrl) &&
+      parseUrl.length === 4 &&
+      parseUrl[3])
+  ) {
+    console.log('OK')
+    return true;
+  }
+  return false;
+};
